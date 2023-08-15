d9de9f8d1a289bf62b653811d6841e6251a472f4
ecro <ecro98.98@gmail.com>
2023-08-15 16:59:15 -0500

    Initial commit

---
+++ README.mk
@@ -0,0 +1,31 @@
+# Exportación de Datos a Excel
+
+Este proyecto web te permite exportar datos de una tabla HTML a un archivo de Excel utilizando las bibliotecas xlsx, file-saverjs y tableexport.
+
+## Instalación
+
+No se requiere instalación para este proyecto, ya que utiliza bibliotecas externas alojadas en línea.
+
+## Uso
+
+1. Abre el archivo `index.html` en tu navegador web.
+2. Verás una tabla con datos de ejemplo y un botón "Exportar datos a Excel".
+3. Haz clic en el botón para exportar los datos de la tabla a un archivo de Excel.
+
+## Código JavaScript
+
+El archivo `main.js` contiene el código JavaScript que controla la exportación de datos. Utiliza la biblioteca TableExport para realizar la exportación.
+
+## Recursos Externos
+
+- Biblioteca xlsx: [https://github.com/SheetJS/sheetjs](https://github.com/SheetJS/sheetjs)
+- Biblioteca file-saverjs: [https://github.com/eligrey/FileSaver.js/](https://github.com/eligrey/FileSaver.js/)
+- Biblioteca tableexport: [https://github.com/clarketm/TableExport](https://github.com/clarketm/TableExport)
+
+## Contribuciones
+
+Si deseas contribuir a este proyecto puedes enviar pull requests con mejoras o correcciones.
+
+---
+
+Creado por [Edwin Romero](https://github.com/gitbatmanhub)
